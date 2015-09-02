c34a4e8756bdd776821bf337b51686acbc1c2821
Andre Machado <csantos.machado@gmail.com>
2015-09-01 23:37:53 -0300

    Initialize repository

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+	$("nav a").click(function(evn){
+        evn.preventDefault();
+        $('html,body').scrollTo(this.hash, this.hash, {duration: 3000}); 
+    });
+
+    $( "#port-image1" ).hover(
+	  function() {
+	    $(".port-holder1").slideDown(".port-text");
+	  }, function() {
+	    $(".port-holder1").slideUp(".port-text");
+	  }
+	);
+
+	$( "#port-image2" ).hover(
+	  function() {
+	    $(".port-holder2").slideDown(".port-text");
+	  }, function() {
+	    $(".port-holder2").slideUp(".port-text");
+	  }
+	);
+
+    $( "#port-image3" ).hover(
+	  function() {
+	    $(".port-holder3").slideDown(".port-text");
+	  }, function() {
+	    $(".port-holder3").slideUp(".port-text");
+	  }
+	);
+});
+
